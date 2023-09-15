293a17c3026aad319c090a63199cc81769b48388
muzzy0743801 <muxxamil0743801@gmail.com>
2023-09-15 17:57:26 +0500

    Add files via upload

---
+++ coal and os/lab 1/lab 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+	int a,b,c;
+
+
+	printf("\nPlease enter two add numbers\n");
+	
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("your sum numbers is: %d",c);
+	
+	return 0;
+	
+}
